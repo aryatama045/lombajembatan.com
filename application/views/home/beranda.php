a33eeeb71dfd1cbdd7cda6ee117a5f49bb1d5077
Aryatama <18211338+aryatama045@users.noreply.github.com>
2022-11-05 00:05:24 +0700

    update fixxx

--- application/views/home/beranda.php
+++ application/views/home/beranda.php
@@ -156,17 +156,17 @@ $iden = $this->db->query("SELECT * FROM tb_web_identitas where id_identitas='1'"
                     
                     
                 </div>    
-                <!-- <div class="tm-hero-text-container-inner4 d-lg-none ">
+                <div class="tm-hero-text-container-inner4 ">
                     <img  src="<?= base_url('assets/home/') ?>center4.png" alt="">
-                </div>  -->
+                </div> 
                 <div class="tm-hero-text-container-inner3 ">
                 <!--d-md-block d-none-->
                 <img src="<?= base_url('assets/home/') ?>center222.png" alt="">
                 </div> 
                 <div class="tm-hero-text-container-inner5">
-                    <div class="img-center-5">
-                        <!-- <img  src="<?= base_url('assets/home/') ?>center4.png" alt=""> -->
-                    </div>
+                    <!-- <div class="img-center-5"> -->
+                        <img  src="<?= base_url('assets/home/') ?>center5.png" alt="">
+                    <!-- </div> -->
                 </div>
 
             </div>
